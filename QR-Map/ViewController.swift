506422095553d8107b65523fadb822c9f05fcd37
Vicwell28 <vicwell.bj@gmail.com>
2023-03-22 15:10:06 -0600

    init

---
+++ QR-Map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QR-Map
+//
+//  Created by soliduSystem on 22/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
