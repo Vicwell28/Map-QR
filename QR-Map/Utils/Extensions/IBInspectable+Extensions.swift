79c552da2b3c1967b322f19cc752656f88d660ef
Vicwell28 <vicwell.bj@gmail.com>
2023-03-24 15:54:23 -0600

    Layout, added user localization

---
+++ QR-Map/Utils/Extensions/IBInspectable+Extensions.swift
@@ -0,0 +1,82 @@
+//
+//  IBInspectable+Extensions.swift
+//  QR-Map
+//
+//  Created by soliduSystem on 22/03/23.
+//
+
+import UIKit
+
+extension UIView {
+    @IBInspectable var shadowColor : UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            layer.shadowColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowOpacity : Float{
+        get {
+            return layer.shadowOpacity
+        }
+        set{
+            layer.shadowOpacity = newValue
+        }
+    }
+    @IBInspectable var shadowOffset : CGSize{
+        get {
+            return layer.shadowOffset
+        }
+        set{
+            layer.shadowOffset = newValue
+        }
+    }
+    @IBInspectable var maskToBounds : Bool{
+        get {
+            return layer.masksToBounds
+        }
+        set{
+            layer.masksToBounds = newValue
+        }
+    }
+    @IBInspectable var cornerRadius : CGFloat{
+        get {
+            return layer.cornerRadius
+        }
+        set{
+            layer.cornerRadius = newValue
+        }
+    }
+    @IBInspectable var borderWidth : CGFloat{
+        get {
+            return layer.borderWidth
+        }
+        set {
+            layer.borderWidth =  newValue
+        }
+    }
+    @IBInspectable var borderColor : UIColor? {
+        get {
+            if let color = layer.borderColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            layer.borderColor = newValue?.cgColor
+        }
+    }
+    @IBInspectable var shadowRadius : CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+}
+
